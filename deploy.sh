9a3dad99e16c856632f9e92729d9e58507e69c75
breneser <brhneser@gmail.com>
2017-01-09 19:30:51 +0000

    Add formatting to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,39 +2,39 @@
 echo "Deploying..."
 
 function verify {
-[ -z "$BUCKET" ] && echo "No S3 bucket specified for deployment, please set BUCKET" && exit 1
-if [ -z "$REGION" ]; then
-  echo "AWS region not set, using eu-west-1" && REGION="eu-west-1"
-else
-  echo "S3 region set as $REGION"
-fi
-if [ -z "$BUILD_FOLDER" ]; then
-  echo "Build folder is not set, using ./build" && BUILD_FOLDER="build"
-else
-  echo "Build folder is $BUILD_FOLDER"
-fi
+    [ -z "$BUCKET" ] && echo "No S3 bucket specified for deployment, please set BUCKET" && exit 1
 
+    if [ -z "$REGION" ]; then
+      echo "AWS region not set, using eu-west-1" && REGION="eu-west-1"
+    else
+      echo "S3 region set as $REGION"
+    fi
+
+    if [ -z "$BUILD_FOLDER" ]; then
+      echo "Build folder is not set, using ./build" && BUILD_FOLDER="build"
+    else
+      echo "Build folder is $BUILD_FOLDER"
+    fi
 }
 
 
 function deploy {
-echo "Deploying build to S3 bucket $BUCKET"
+    echo "Deploying build to S3 bucket $BUCKET"
 
-#directory of this script
+    #directory of this script
 
-if [ "$NO_ROBOTS" = "true" ]; then
-	dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-	echo "No robots flag set for this deployment, deploying no robots configuration"
-	cp "$dir"/norobots.txt "$PWD"/"$BUILD_FOLDER"/robots.txt
-fi
+    if [ "$NO_ROBOTS" = "true" ]; then
+        dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+        echo "No robots flag set for this deployment, deploying no robots configuration"
+        cp "$dir"/norobots.txt "$PWD"/"$BUILD_FOLDER"/robots.txt
+    fi
 
-aws s3 sync "$PWD"/build s3://"$BUCKET" --region="$REGION" --delete --storage-class REDUCED_REDUNDANCY
+    aws s3 sync "$PWD"/build s3://"$BUCKET" --region="$REGION" --delete --storage-class REDUCED_REDUNDANCY
 
-#Clean no robots text file
-if [ "$NO_ROBOTS" = "true" ]; then
-	rm "$PWD"/build/robots.txt
-fi
-
+    #Clean no robots text file
+    if [ "$NO_ROBOTS" = "true" ]; then
+        rm "$PWD"/build/robots.txt
+    fi
 }
 
 verify && deploy
